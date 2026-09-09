3ab9d862a25861a300f1683fbe6c002ad0983ec7
ndeyecoumbathiam2004-eng <ndeyecoumbathiam2004@gmail.com>
2026-09-09 14:37:22 +0000

    Delete AdherentRepository.java

--- AdherentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Adherent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdherentRepository extends JpaRepository<Adherent, Long> {
-}
